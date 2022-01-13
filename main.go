a6d6b107b40ac7c4ada8b57ed5cff08aeb073022
ariary <ariary9.2@hotmail.fr>
2022-01-13 05:58:22 -0500

    Take into account when response is chunked + refactoring

--- main.go
+++ main.go
@@ -2,158 +2,193 @@
 package main
 
 import (
-    "net"
-    "net/http"
-    "net/url"
-    //"crypto/tls"
-    "fmt"
-    "flag"
-    "log"
-    "os"
-    "bufio"
-    "time"
-    "strconv"
-    "github.com/fatih/color"
+	"io"
+	"net"
+	"net/http"
+	"net/url"
+
+	//"crypto/tls"
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/fatih/color"
 )
 
-var headers = []string{"Client-IP","Connection","Contact","Forwarded","From","Host","Origin","Referer","True-Client-IP","X-Client-IP","X-Custom-IP-Authorization","X-Forward-For","X-Forwarded-For","X-Forwarded-Host","X-Forwarded-Server","X-Host","X-HTTP-Host-Override","X-Original-URL","X-Originating-IP","X-Real-IP","X-Remote-Addr","X-Remote-IP","X-Rewrite-URL","X-Wap-Profile"}
-var inject = []string{"127.0.0.1","localhost","0.0.0.0","0","127.1","127.0.1","2130706433"}
+var headers = []string{"Client-IP", "Connection", "Contact", "Forwarded", "From", "Host", "Origin", "Referer", "True-Client-IP", "X-Client-IP", "X-Custom-IP-Authorization", "X-Forward-For", "X-Forwarded-For", "X-Forwarded-Host", "X-Forwarded-Server", "X-Host", "X-HTTP-Host-Override", "X-Original-URL", "X-Originating-IP", "X-Real-IP", "X-Remote-Addr", "X-Remote-IP", "X-Rewrite-URL", "X-Wap-Profile"}
+var inject = []string{"127.0.0.1", "localhost", "0.0.0.0", "0", "127.1", "127.0.1", "2130706433"}
 var urlt string
 var pfile string
 var to int
 
-func payloadInject() {
-    timeout := time.Duration(to * 1000000)
-    g := color.New(color.FgGreen)
-    r := color.New(color.FgRed)
-    var tr = &http.Transport{
-		MaxIdleConns:      30,
-		IdleConnTimeout:   time.Second,
-		DisableKeepAlives: true,
-        //TLSClientConfig:   &tls.Config{InsecureSkipVerify: true},
-		DialContext: (&net.Dialer{
-			Timeout:   timeout,
-			KeepAlive: time.Second,
-		}).DialContext,
+// ResolveContentLength returns right content-length value. First it re-make the request with chunked-compression
+// disable, then it computes Content-Length from body length. If disabling encoding is efficient, client is modified for
+// future requests also
+func ResolveContentLength(tr *http.Transport, client *http.Client, url string, headerName string, headerValue string) (contentLentgh int64, err error) {
+	//first disable chunked-compression
+	tr.DisableCompression = true
+	client.Transport = tr
+
+	resp, _ := GetRequest(tr, client, url, headerName, headerValue)
+	contentLentgh = resp.ContentLength
+
+	if contentLentgh == -1 {
+		tr.DisableCompression = false
+		client.Transport = tr
+
+		//compute Content-Length by ourself
+		defer resp.Body.Close()
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return -1, err
+		}
+		contentLentgh = int64(len(bodyBytes))
 	}
-	client := &http.Client{
-		Transport:     tr,
-		Timeout:       timeout,
-	}
-
-    // open and iterate
-    file, err := os.Open(pfile)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
-    scanner := bufio.NewScanner(file)
-
-    // baseline request - gauge normal response
-    breq, err := http.Get(urlt)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    // loop through payload file and inject
-    for scanner.Scan() {
-        for _, header := range headers {
-            req, err := http.NewRequest("GET", urlt, nil)
-            req.Header.Set(header, scanner.Text())
-            resp, err := client.Do(req)
-            if err != nil {
-                continue
-            }
-            if breq.ContentLength != resp.ContentLength {
-                g.Println("[+] "+"["+urlt+"]"+" "+"["+header+": "+scanner.Text()+"]"+" "+"[Code: "+strconv.Itoa(int(resp.StatusCode))+"] "+"[Size: "+ strconv.Itoa(int(resp.ContentLength))+"]")
-            } else {
-                r.Println("[-] "+"["+urlt+"]"+" "+"["+header+": "+scanner.Text()+"]"+" "+"[Code: "+strconv.Itoa(int(resp.StatusCode))+"] "+"[Size: "+ strconv.Itoa(int(resp.ContentLength))+"]")
-            }
-            defer resp.Body.Close()
-        }
-    }
+	return contentLentgh, err
 }
 
-func headerInject() {
-    timeout := time.Duration(to * 1000000)
-    g := color.New(color.FgGreen)
-    r := color.New(color.FgRed)
-    var tr = &http.Transport{
+//GetRequest performs the Get request giving an URL & Header info, and return the http.Response struct
+func GetRequest(tr *http.Transport, client *http.Client, url string, headerName string, headerValue string) (resp *http.Response, err error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return resp, err
+	}
+
+	if headerName != "" {
+		req.Header.Set(headerName, headerValue)
+	}
+
+	resp, err = client.Do(req)
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, err
+}
+
+//InitBaseline performs baseline request and retrieve baselines valuesfrom it (content-length , client used, etc)
+func InitBaseline() (tr *http.Transport, client *http.Client, bresp *http.Response) {
+	timeout := time.Duration(to * 1000000)
+	tr = &http.Transport{
 		MaxIdleConns:      30,
 		IdleConnTimeout:   time.Second,
 		DisableKeepAlives: true,
-        //TLSClientConfig:   &tls.Config{InsecureSkipVerify: true},
+		//TLSClientConfig:   &tls.Config{InsecureSkipVerify: true},
 		DialContext: (&net.Dialer{
 			Timeout:   timeout,
 			KeepAlive: time.Second,
 		}).DialContext,
 	}
-	client := &http.Client{
-		Transport:     tr,
-		Timeout:       timeout,
+
+	client = &http.Client{
+		Transport: tr,
+		Timeout:   timeout,
 	}
 
-    // baseline request - gauge normal response
-    breq, err := http.Get(urlt)
-    if err != nil {
-        log.Fatal(err)
-    }
+	//baseline request - gauge normal response
+	bresp, err := GetRequest(tr, client, urlt, "", "")
+	if err != nil {
+		log.Fatal("Failed to make baseline request:", err)
+	}
 
-    // loop through default payloads and inject
-    for _, header := range headers {
-        for _, i := range inject {
-            req, err := http.NewRequest("GET", urlt, nil)
-            req.Header.Set(header, i)
-            resp, err := client.Do(req)
-            if err != nil {
-                continue
-            }
-            if breq.ContentLength != resp.ContentLength {
-                g.Println("[+] "+"["+urlt+"]"+" "+"["+header+": "+i+"]"+" "+"[Code: "+strconv.Itoa(int(resp.StatusCode))+"] "+"[Size: "+ strconv.Itoa(int(resp.ContentLength))+"]")
-            } else {
-                r.Println("[-] "+"["+urlt+"]"+" "+"["+header+": "+i+"]"+" "+"[Code: "+strconv.Itoa(int(resp.StatusCode))+"] "+"[Size: "+ strconv.Itoa(int(resp.ContentLength))+"]")
-            }
-            defer resp.Body.Close()
-        }
-    }
+	return tr, client, bresp
+}
+
+//HeaderInject performs a baseline request, retrieve content-lenght from it.
+//Then it performs multiples request with various header and compare response Content-Length
+//w/ baseline response
+func HeaderInject() {
+	g := color.New(color.FgGreen)
+	r := color.New(color.FgRed)
+
+	tr, client, bresp := InitBaseline()
+	bContentLength := bresp.ContentLength
+
+	if bContentLength == -1 { //the length is unknown
+		var err error
+		bContentLength, err = ResolveContentLength(tr, client, urlt, "", "")
+		if err != nil {
+			fmt.Println("Failed to retrieve Content-Lenght:", err)
+		}
+	}
+
+	//loop through default payloads and inject
+	for _, header := range headers {
+		for _, i := range inject {
+			resp, err := GetRequest(tr, client, urlt, header, i)
+			if err != nil {
+				continue
+			}
+
+			contentLength := resp.ContentLength
+			if contentLength == -1 { //the length is unknown
+				contentLength, err = ResolveContentLength(tr, client, urlt, header, i)
+				if err != nil {
+					continue
+				}
+			}
+
+			if bContentLength != contentLength {
+				g.Println("[+] " + "[" + urlt + "]" + " " + "[" + header + ": " + i + "]" + " " + "[Code: " + strconv.Itoa(int(resp.StatusCode)) + "] " + "[Size: " + strconv.Itoa(int(contentLength)) + "]")
+			} else {
+				r.Println("[-] " + "[" + urlt + "]" + " " + "[" + header + ": " + i + "]" + " " + "[Code: " + strconv.Itoa(int(resp.StatusCode)) + "] " + "[Size: " + strconv.Itoa(int(contentLength)) + "]")
+			}
+			defer resp.Body.Close()
+		}
+	}
 }
 
 func init() {
-    flag.Usage = func() {
-        f := "Usage:\n"
-        f += "  headi -u https://target.com/resource\n"
-        f += "  headi -u https://target.com/resource -p internal_addrs.txt\n\n"
-        f += "Options:\n"
-        f += "  -p, --pfile <file>       Payload File\n"
-        f += "  -t, --timeout <millis>   HTTP Timeout\n"
-        f += "  -u, --url <url>          Target URL\n"
-        fmt.Fprintf(os.Stderr, f)
-    }
+	flag.Usage = func() {
+		f := "Usage:\n"
+		f += "  headi -u https://target.com/resource\n"
+		f += "  headi -u https://target.com/resource -p internal_addrs.txt\n\n"
+		f += "Options:\n"
+		f += "  -p, --pfile <file>       Payload File\n"
+		f += "  -t, --timeout <millis>   HTTP Timeout\n"
+		f += "  -u, --url <url>          Target URL\n"
+		fmt.Fprintf(os.Stderr, f)
+	}
 }
 
 func main() {
-    flag.StringVar(&urlt, "url", "", "")
-    flag.StringVar(&urlt, "u", "", "")
-    flag.StringVar(&pfile, "pfile","","")
-    flag.StringVar(&pfile, "p","","")
-    flag.IntVar(&to, "timeout", 10000, "")
-    flag.IntVar(&to, "t", 10000, "")
-    flag.Parse()
-    if urlt == "" {
-        flag.Usage()
-    } else {
-        u, err := url.Parse(urlt)
-        if err != nil {
-            log.Fatal(err)
-        }
-        if u.Scheme == "" || u.Host == "" || u.Path == "" {
-            fmt.Println("Invalid URL: ",urlt)
-            os.Exit(1)
-        }
-        if pfile != "" {
-            payloadInject()
-        } else {
-            headerInject()
-        }
-    }
+	flag.StringVar(&urlt, "url", "", "url to fetch and to which perform header injections")
+	flag.StringVar(&urlt, "u", "", "url to fetch and to which perform header injections")
+	flag.StringVar(&pfile, "pfile", "", "Give a file with custom header value (useful to provide internal IP")
+	flag.StringVar(&pfile, "p", "", "Give a file with custom header value (useful to provide internal IP")
+	flag.IntVar(&to, "timeout", 10000, "Custom HTTP timeout")
+	flag.IntVar(&to, "t", 10000, "")
+	flag.Parse()
+	if urlt == "" {
+		flag.Usage()
+	} else {
+		u, err := url.Parse(urlt)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if u.Scheme == "" || u.Host == "" || u.Path == "" {
+			fmt.Println("Invalid URL: ", urlt)
+			os.Exit(1)
+		}
+
+		if pfile != "" {
+			//Reconstruct inject []string, containaing header value
+			inject = nil
+			// open and iterate
+			file, err := os.Open(pfile)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer file.Close()
+			scanner := bufio.NewScanner(file)
+			for scanner.Scan() {
+				inject = append(inject, scanner.Text())
+			}
+		}
+		HeaderInject()
+	}
 }
